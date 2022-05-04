208c45c311bda55949df80de0e9981ad21bba3ed
Kathy Yu <fyu9@stanford.edu>
2022-05-04 12:33:47 -0700

    Resolved dependencies, added adjustable parameters, cleaned up

--- CXRMetric/run_eval.py
+++ CXRMetric/run_eval.py
@@ -1,47 +1,65 @@
-import pandas as pd
+import json
 import numpy as np
 import os
-
-from fast_bleu import BLEU
-from bert_score import BERTScorer
-from CXRMetric.radgraph_evaluate_model import run_radgraph
-import torch
-import json
+import pandas as pd
 import pickle
+import torch
+
+from bert_score import BERTScorer
+from fast_bleu import BLEU
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 
-REPORT_COL_NAME ="report"
-# TODO: link these in our readme
-CHEXBERT_PATH = ""
-RADGRAPH_PATH = "/deep/group/radgraph/physionet.org/files/radgraph/1.0.0/models/model_checkpoint/model.tar.gz"
-weights = {'bigram': (1/2., 1/2.)}
+import config
+from CXRMetric.radgraph_evaluate_model import run_radgraph
+
+"""Computes 4 individual metrics and a composite metric on radiology reports."""
+
+
+CHEXBERT_PATH = config.CHEXBERT_PATH
+RADGRAPH_PATH = config.RADGRAPH_PATH
+
+REPORT_COL_NAME = "report"
+STUDY_ID_COL_NAME = "study_id"
+COLS = ["radgraph_combined", "bertscore", "semb_score", "bleu_score"]
+
+cache_path = "cache/"
+pred_embed_path = os.path.join(cache_path, "pred_embeddings.pt")
+gt_embed_path = os.path.join(cache_path, "gt_embeddings.pt")
+weights = {"bigram": (1/2., 1/2.)}
+composite_metric_col = "cxr_metric_score"
+linear_model_path = "CXRMetric/lin_score_model.pkl"
+
 
 def prep_reports(reports):
-    return [list(filter(lambda val: val !=  "", str(elem).lower().replace(".", " .").split(" "))) for elem in reports]
+    return [list(filter(
+        lambda val: val !=  "", str(elem)\
+            .lower().replace(".", " .").split(" "))) for elem in reports]
 
 # adds a column with the bleu score for each report
 def add_bleu_col(gt_df, pred_df):
     pred_df["bleu_score"] = [0.0] * len(pred_df)
     for i, row in gt_df.iterrows():
-        gt_report = prep_reports([row['report']])[0]
-        pred_row = pred_df[pred_df['study_id'] == row['study_id']]
+        gt_report = prep_reports([row[REPORT_COL_NAME]])[0]
+        pred_row = pred_df[pred_df[STUDY_ID_COL_NAME] == row[STUDY_ID_COL_NAME]]
         if len(pred_row) == 0:
             print("problem")
             continue
-        predicted_report = prep_reports([pred_row['report'].values[0]])[0]
+        predicted_report = \
+            prep_reports([pred_row[REPORT_COL_NAME].values[0]])[0]
         if len(pred_row) == 1:
             bleu = BLEU([gt_report], weights)
-            score = bleu.get_score([predicted_report])['bigram']
+            score = bleu.get_score([predicted_report])["bigram"]
             assert len(score) == 1
-            _index = pred_df.index[pred_df['study_id']==row['study_id']].tolist()[0]
+            _index = pred_df.index[
+                pred_df[STUDY_ID_COL_NAME]==row[STUDY_ID_COL_NAME]].tolist()[0]
             pred_df.at[_index, "bleu_score"] = score[0]
     return pred_df
 
 def add_bertscore_col(gt_df, pred_df):
-    test_reports = gt_df["report"].tolist()
+    test_reports = gt_df[REPORT_COL_NAME].tolist()
     test_reports = [test.lstrip() for test in test_reports]
-    method_reports = pred_df["report"].tolist()
+    method_reports = pred_df[REPORT_COL_NAME].tolist()
     method_reports = [report.lstrip() for report in method_reports]
 
     scorer = BERTScorer(model_type="distilroberta-base", batch_size=256)
@@ -57,7 +75,8 @@ def add_semb_col(pred_df, semb_path, gt_path):
     print(len(pred_df), len(np_pred_embeds), len(np_label_embeds))
     scores = []
     for i, (label, pred) in enumerate(zip(np_label_embeds, np_pred_embeds)):
-        sim_scores = (label * pred).sum()/(np.linalg.norm(label)*np.linalg.norm(pred))
+        sim_scores = (label * pred).sum() / (
+            np.linalg.norm(label) * np.linalg.norm(pred))
         scores.append(sim_scores)
     pred_df["semb_score"] = scores
     return pred_df
@@ -76,12 +95,12 @@ def add_radgraph_col(pred_df, entities_path, relations_path):
     radgraph_scores = []
     count = 0
     for i, row in pred_df.iterrows():
-        radgraph_scores.append(study_id_to_radgraph[int(row['study_id'])])
+        radgraph_scores.append(study_id_to_radgraph[int(row[STUDY_ID_COL_NAME])])
     pred_df["radgraph_combined"] = radgraph_scores
     return pred_df
 
 def calc_metric(gt_csv, pred_csv, out_csv): # TODO: support single metrics at a time
-    os.environ['MKL_THREADING_LAYER'] = 'GNU'
+    os.environ["MKL_THREADING_LAYER"] = "GNU"
     print(gt_csv, pred_csv)
     # take a csv to the eval an gt reports
     gt, pred = pd.read_csv(gt_csv), pd.read_csv(pred_csv)
@@ -98,24 +117,24 @@ def calc_metric(gt_csv, pred_csv, out_csv): # TODO: support single metrics at a 
 
     # run encode.py to make the semb column
     print(os.getcwd())
-    pred_embed_path = "cache/pred_embeddings.pt"
-    gt_embed_path = "cache/gt_embeddings.pt"
-    os.system(f"python CXRMetric/CheXbert/src/encode.py -c CXRMetric/CheXbert/models/chexbert.pth -d {pred_csv} -o {pred_embed_path}")
-    os.system(f"python CXRMetric/CheXbert/src/encode.py -c CXRMetric/CheXbert/models/chexbert.pth -d {gt_csv} -o {gt_embed_path}")
+    os.system(f"mkdir -p {cache_path}")
+    os.system(f"python CXRMetric/CheXbert/src/encode.py -c {CHEXBERT_PATH} -d {pred_csv} -o {pred_embed_path}")
+    os.system(f"python CXRMetric/CheXbert/src/encode.py -c {CHEXBERT_PATH} -d {gt_csv} -o {gt_embed_path}")
     pred = add_semb_col(pred, pred_embed_path, gt_embed_path)
 
     # run radgraph to create that column
-    entities_path, relations_path = run_radgraph(gt_csv, pred_csv, "cache/")
-    entities_path, relations_path = "cache/entities_cache.json", "cache/relations_cache.json"
+    entities_path = os.path.join(cache_path, "entities_cache.json")
+    relations_path = os.path.join(cache_path, "relations_cache.json")
+    run_radgraph(gt_csv, pred_csv, cache_path, RADGRAPH_PATH,
+                 entities_path, relations_path)
     pred = add_radgraph_col(pred, entities_path, relations_path)
 
     # run the linear model
-    model_file = open('CXRMetric/lin_score_model.pkl', 'rb')
+    model_file = open(linear_model_path, "rb")
     lin_model= pickle.load(model_file)
     model_file.close()
     # normalize
-    cols = ["radgraph_combined", "bertscore", "semb_score", "bleu_score"]
-    input_data = np.array(pred[cols])
+    input_data = np.array(pred[COLS])
     scaler = MinMaxScaler()
     scaler.fit(input_data)
     norm_input_data = scaler.transform(input_data)
@@ -123,8 +142,7 @@ def calc_metric(gt_csv, pred_csv, out_csv): # TODO: support single metrics at a 
     scores = lin_model.predict(norm_input_data)
 
     # append new column
-    pred["cxr_metric_score"] = scores
+    pred[composite_metric_col] = scores
 
     # save results in the out folder
     pred.to_csv(out_csv)
-
---
+++ config.py
@@ -0,0 +1,8 @@
+# Model checkpoints
+CHEXBERT_PATH = "CheXbert/models/chexbert.pth"
+RADGRAPH_PATH = "radgraph/physionet.org/files/radgraph/1.0.0/models/model_checkpoint/model.tar.gz"
+
+# Report paths
+GT_REPORTS = "reports/gt_reports.csv"
+PREDICTED_REPORTS = "reports/predicted_reports.csv"
+OUT_FILE = "report_scores.csv"
