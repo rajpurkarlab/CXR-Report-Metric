c2d3ba669bfb2aa12f7b33e67e88c65bc93b247a
Kathy Yu <fyu9@stanford.edu>
2022-05-26 10:28:17 -0700

    Updated composite metric

--- CXRMetric/run_eval.py
+++ CXRMetric/run_eval.py
@@ -23,6 +23,7 @@ RADGRAPH_PATH = config.RADGRAPH_PATH
 REPORT_COL_NAME = "report"
 STUDY_ID_COL_NAME = "study_id"
 COLS = ["radgraph_combined", "bertscore", "semb_score", "bleu_score"]
+COMPOSITE_METRIC_COLS = ["radgraph_combined", "bleu_score"]
 COMPOSITE_METRIC_PATH = "CXRMetric/composite_metric_model.pkl"
 NORMALIZER_PATH = "CXRMetric/normalizer.pkl"
 
@@ -34,19 +35,17 @@ composite_metric_col = "cxr_metric_score"
 
 
 def prep_reports(reports):
+    """Preprocesses reports"""
     return [list(filter(
         lambda val: val !=  "", str(elem)\
             .lower().replace(".", " .").split(" "))) for elem in reports]
 
-# adds a column with the bleu score for each report
 def add_bleu_col(gt_df, pred_df):
+    """Computes BLEU-2 and adds scores as a column to prediction df."""
     pred_df["bleu_score"] = [0.0] * len(pred_df)
     for i, row in gt_df.iterrows():
         gt_report = prep_reports([row[REPORT_COL_NAME]])[0]
         pred_row = pred_df[pred_df[STUDY_ID_COL_NAME] == row[STUDY_ID_COL_NAME]]
-        if len(pred_row) == 0:
-            print("problem")
-            continue
         predicted_report = \
             prep_reports([pred_row[REPORT_COL_NAME].values[0]])[0]
         if len(pred_row) == 1:
@@ -59,6 +58,7 @@ def add_bleu_col(gt_df, pred_df):
     return pred_df
 
 def add_bertscore_col(gt_df, pred_df):
+    """Computes BERTScore and adds scores as a column to prediction df."""
     test_reports = gt_df[REPORT_COL_NAME].tolist()
     test_reports = [re.sub(r' +', ' ', test) for test in test_reports]
     method_reports = pred_df[REPORT_COL_NAME].tolist()
@@ -74,11 +74,11 @@ def add_bertscore_col(gt_df, pred_df):
     return pred_df
 
 def add_semb_col(pred_df, semb_path, gt_path):
+    """Computes s_emb and adds scores as a column to prediction df."""
     label_embeds = torch.load(gt_path)
     pred_embeds = torch.load(semb_path)
     np_label_embeds = torch.stack([*label_embeds.values()], dim=0).numpy()
     np_pred_embeds = torch.stack([*pred_embeds.values()], dim=0).numpy()
-    # print(len(pred_df), len(np_pred_embeds), len(np_label_embeds))
     scores = []
     for i, (label, pred) in enumerate(zip(np_label_embeds, np_pred_embeds)):
         sim_scores = (label * pred).sum() / (
@@ -88,6 +88,7 @@ def add_semb_col(pred_df, semb_path, gt_path):
     return pred_df
 
 def add_radgraph_col(pred_df, entities_path, relations_path):
+    """Computes RadGraph F1 and adds scores as a column to prediction df."""
     study_id_to_radgraph = {}
     with open(entities_path, "r") as f:
         scores = json.load(f)
@@ -112,9 +113,9 @@ def add_radgraph_col(pred_df, entities_path, relations_path):
     return pred_df
 
 def calc_metric(gt_csv, pred_csv, out_csv): # TODO: support single metrics at a time
+    """Computes four metrics and composite metric scores."""
     os.environ["MKL_THREADING_LAYER"] = "GNU"
-    # print(gt_csv, pred_csv)
-    # take a csv to the eval an gt reports
+
     gt = pd.read_csv(gt_csv)\
         .sort_values(by=[STUDY_ID_COL_NAME]).reset_index(drop=True)
     gt.to_csv(gt_csv)
@@ -134,7 +135,6 @@ def calc_metric(gt_csv, pred_csv, out_csv): # TODO: support single metrics at a 
     pred = add_bertscore_col(gt, pred)
 
     # run encode.py to make the semb column
-    # print(os.getcwd())
     os.system(f"mkdir -p {cache_path}")
     os.system(f"python CXRMetric/CheXbert/src/encode.py -c {CHEXBERT_PATH} -d {pred_csv} -o {pred_embed_path}")
     os.system(f"python CXRMetric/CheXbert/src/encode.py -c {CHEXBERT_PATH} -d {gt_csv} -o {gt_embed_path}")
@@ -147,17 +147,16 @@ def calc_metric(gt_csv, pred_csv, out_csv): # TODO: support single metrics at a 
                  entities_path, relations_path)
     pred = add_radgraph_col(pred, entities_path, relations_path)
 
-    # run the linear model
+    # compute composite metric
     with open(COMPOSITE_METRIC_PATH, "rb") as f:
         composite_metric_model = pickle.load(f)
     with open(NORMALIZER_PATH, "rb") as f:
         normalizer = pickle.load(f)
     # normalize
-    input_data = np.array(pred[COLS])
+    input_data = np.array(pred[COMPOSITE_METRIC_COLS])
     norm_input_data = normalizer.transform(input_data)
     # generate new col
     scores = composite_metric_model.predict(norm_input_data)
-
     # append new column
     pred[composite_metric_col] = scores
 
